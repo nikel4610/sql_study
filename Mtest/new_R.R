39e3c8e108bd8da277f08b6f3e41539aadb587f3
Hyun-woo <nikel4610@gmail.com>
2022-07-12 16:33:38 +0900

    more dataframe and ifelse:

--- Mtest/new_R.R
+++ Mtest/new_R.R
@@ -7,18 +7,32 @@ df_midterm
 
 df_midterm$english
 
-install.packages('readxl')
+# install.packages('readxl')
 library(readxl)
+library(dplyr)
 df_exam = read_excel('D:/rproject/sql_study/Mtest/excel_exam.xlsx')
-head(df_exam)
-str(df_exam)
+df_exam
 
-df_csv = read.csv('D:/rproject/sql_study/Mtest/csv_exam.csv', header = T)
-df_csv
+df_exam %>% filter(class == 3)
+df_exam %>% filter(class == 2 | class == 3)
+df_exam %>% filter(class %in% c(1, 3, 5))
 
-str(df_csv)
-dim(df_csv)
-summary(df_csv)
-head(df_csv) # 기본 6행으로 출력됨
-head(df_csv, 10) # 10행 출력
+df_exam
+df_exam %>% filter(math>=60)
+df_exam
+df_exam %>% select(english)
+df_exam
 
+# df_exam$total = (df_exam$math) + (df_exam$english) + (df_exam$science)
+df_exam %>% mutate(hap = math + english + science, round(hap/3, 2))
+df_exam
+
+# 5행에서 7행만 표시
+df_exam[c(5:7), ]
+df_exam[5:7, ]
+
+df_exam
+df_exam[15:17, 4] #[행, 열]
+
+df_exam$test = ifelse(df_exam$total > 200, '합격', '재시험')
+df_exam
